444fec65aa4870d351a5d9033f7987ca59d61d3c
John Eldredge <jeldredge@regis.edu>
2022-03-27 18:21:55 -0600

    Rename folders for organization

---
+++ Week2/README.md
@@ -0,0 +1,2 @@
+This weeks update include folder organization, style updates and javascript addition.
+I have also created the sekeleton for a login page.
---
+++ Week2/src/app.js
@@ -0,0 +1,48 @@
+class Soap{
+    name;
+    premade = false;
+    custom = true;
+    ingredients = new Map();
+
+    constructor(name, premade, custom, ingredients){
+        this.name = name;
+        this.premade = premade;
+        this.custom = custom;
+        this.ingredients = ingredients;
+    }
+}
+
+//The soap to create
+newSoap = new Map();
+
+//All soaps conatin these base ingredients
+baseSoap = new Map([
+    ["lye", 8.97],
+    ["water", 12.80],
+    ["castor", 9.60]
+])
+
+/*Add the base ingredients to each soap
+baseSoap.array.forEach(function(value, key) {
+    newSoap.set(key, value);
+});*/
+
+//This at least works
+for (var x of baseSoap.entries()){
+    newSoap.set(x[0], x[1]);
+}
+
+//Add the user defined ingredients to cureent soap
+//newSoap.set(baseSoap);
+newSoap.set("aloe butter", 9.60);
+newSoap.set("babassu", 9.60);
+newSoap.set("kokum butter", 6.40);
+newSoap.set("mango butter", 6.40);
+newSoap.set("neem seed oil", 3.20);
+newSoap.set("palm oil", 6.40);
+newSoap.set("safflower oil", 6.40);
+newSoap.set("shea butter", 6.40);
+
+//Ensure the code will work and view in console... Report
+console.log(new Soap("Trenzik", false, true, newSoap));
+console.log("Created soap has " + newSoap.size + " ingredients!");
