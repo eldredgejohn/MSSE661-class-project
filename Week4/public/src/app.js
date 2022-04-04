ee521a4c64f427ec4e251daedaf443bb76573479
John Eldredge <jeldredge@regis.edu>
2022-04-03 20:34:30 -0600

    Week 4 updates from video

--- Week4/public/src/app.js
+++ Week4/public/src/app.js
@@ -1,3 +1,35 @@
+const doLogin = function(e) {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+  
+    login({
+      username: username,
+      password: password
+    }).then(function(res) {
+      window.location.href = 'home.html';
+    });
+  };
+  
+  const doRegister = function(e) {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    register({
+      username: username,
+      email: email,
+      password: password
+    }).then(function(res) {
+      window.location.href = 'home.html';
+    });
+  };
+  
+  const doLogout = function(e) {
+    e.preventDefault();
+  };
+
 class Soap{
     name;
     premade = false;
--- Week4/server/index.js
+++ Week4/server/index.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const app = express();
 
+const port = process.env.port || 4000;
+
 app.use(express.static("public"));
+
 app.use("/css", express.static(__dirname + "/public/css"));
 app.use("/js", express.static(__dirname + "/public/src"));
 app.use("/img", express.static(__dirname + "/public/img"));
 
-/*const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '!Pa33w0rd!',
-  //database: 'database name'
-});
-connection.connect((err) => {
-  if (err) throw err;
-  console.log('Connected!');
-});*/
-
-app.listen(3000, function() {
-    console.log("Server started at http://localhost:%s", 3000);
+app.listen(port, function() {
+    console.log("Server started at http://localhost:%s", port);
 });
